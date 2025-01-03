e8c6beaffc0ae927d42f0d18941131d4a1e6229c
Rohan Acharya <rohanacharya38@gmail.com>
2025-01-03 22:04:01 +0545

    Missing code from previous commit

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets,status
+from rest_framework import viewsets,status,permissions,authentication
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,action
 
-from backend.core.models import Post, PostComment, Travellers, User
-from backend.core.serializers import PostSerializer
+from backend.core.models import Business, Guide, Package, PackageSubscription, Post, PostComment, Travellers, User
+from backend.core.serializers import BusinessSerializer, PackageCommentSerializer, PackageSerializer, PackageSubscriptionSerializer, PostSerializer, TravellersSerializer, UserSerializer
 
 from django.db.models import Count
 
@@ -74,3 +74,253 @@ class PostViewSet(viewsets.ModelViewSet):
         # print(data)
         return Response(status=status.HTTP_200_OK)
         # return super().create(request, *args, **kwargs
+
+class TravellersViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+    permission_classes = []
+    queryset = Travellers.objects.all()
+    serializer_class = TravellersSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        print(kwargs)
+        username = kwargs.get('pk')
+        # traveller = get_object_or_404(Travellers,base_user__username = username)
+        print(username)
+        traveller = Travellers.objects.get(base_user__username = username)
+        serializer = self.serializer_class(traveller)
+        return Response(status=status.HTTP_200_OK,data=serializer.data)
+        
+    @action(
+        methods=["POST"], permission_classes=[], authentication_classes=[], detail=False
+    )
+    def create_user(self, request, *args, **kwargs):
+        data = request.data
+        base_user = {
+            "username": data["username"],
+            "password": data["password"],
+            "email": data["email"],
+        }
+        data = request.data
+        base_user = {
+            "username": data["username"],
+            "password": data["password"],
+            "email": data["email"],
+        }
+        data["base_user"] = base_user
+        interests = data["interests"]
+        data["base_user"] = base_user
+        interests = data["interests"]
+        # print(interests)
+        # traveller_serializer.
+
+        data["interests"] = []
+
+        data["interests"] = []
+        for interest in interests:
+            # print(interest)
+            data["interests"].append({"name": interest})
+
+            data["interests"].append({"name": interest})
+
+        # data['interests']=None
+        # print(data)
+        # print(data['interests'])
+        traveller_serializer = self.serializer_class(data=data)
+        traveller_serializer = self.serializer_class(data=data)
+        traveller_serializer.is_valid(raise_exception=True)
+        traveller = traveller_serializer.save()
+        print(traveller)
+
+
+        return Response(status=status.HTTP_200_OK)
+        # return super().create(request, *args, **kwargs)
+class BusinessViewSet(viewsets.ModelViewSet):
+    authentication_classes = [authentication.BasicAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Business.objects.all()
+    serializer_class = BusinessSerializer
+
+    # @action(
+    #     methods=["POST"], permission_classes=[], authentication_classes=[], detail=False
+    # )
+
+    @action(
+        methods=["POST"], permission_classes=[], authentication_classes=[], detail=False
+    )
+    def create_user(self, request, *args, **kwargs):
+        data = request.data
+        base_user = {"username": data["username"], "password": data["password"]}
+        data["base_user"] = base_user
+        interests = data["interests"]
+        data = request.data
+        base_user = {"username": data["username"], "password": data["password"]}
+        data["base_user"] = base_user
+        interests = data["interests"]
+        # print(interests)
+        # traveller_serializer.
+
+        data["interests"] = []
+
+        data["interests"] = []
+        for interest in interests:
+            # print(interest)
+            data["interests"].append({"name": interest})
+
+            data["interests"].append({"name": interest})
+
+        # data['interests']=None
+        # print(data)
+        # print(data['interests'])
+        traveller_serializer = self.serializer_class(data=data)
+        traveller_serializer = self.serializer_class(data=data)
+        traveller_serializer.is_valid(raise_exception=True)
+        traveller = traveller_serializer.save()
+        print(traveller)
+
+
+        return Response(status=status.HTTP_200_OK)
+        # return super().create(request, *args, **kwargs)
+
+class PackageViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+    permission_classes = []
+    authentication_classes = []
+    permission_classes = []
+    queryset = Package.objects.all()
+    serializer_class = PackageSerializer
+
+    def get_traveller(username):
+        user = User.objects.get(username=username)
+        traveller = Travellers.objects.get(base_user=user.pk)
+        traveller = Travellers.objects.get(base_user=user.pk)
+        return traveller
+
+    @action(methods=["GET"], detail=False)
+    def recommendations(self, request):
+        traveller = self.get_traveller(request.data["username"])
+        matching_users = (
+            Package.objects.filter(label__name__in=traveller.interests)
+            .annotate(matched_labels=Count("label"))
+            .order_by("-matched_labels")
+        )
+
+    @action(methods=["GET"], detail=False)
+    def trending(self, request):
+        traveller = self.get_traveller(request.data["username"])
+
+    @action(methods=["POST","GET"], permission_classes=[], authentication_classes=[], detail=False)
+    def subscribe(self, request, *args, **kwargs):
+        if(request.method=="POST"):
+            data =request.data
+            user = User.objects.get(username=data['username'])
+            # comment = data['comment']
+            package = Package.objects.get(id= data['id'])
+            package_subsctipton_data={
+                
+            }
+            package_subsctipton_data['package']= package.pk
+            package_subsctipton_data['subscribed_by']= user.pk
+            package_subscription_serializer = PackageSubscriptionSerializer(data= package_subsctipton_data)
+            package_subscription_serializer.is_valid(raise_exception=True)
+            package_subscription_serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            data = request.data
+            event_id = data['id']
+            package = Package.objects.get(id= event_id)
+            # interested_users = event.eventinterested_set.all().values_list('interested_user',flat=True)
+            interested_users = User.objects.filter(packagesubscription__package=package)
+            return Response(status=status.HTTP_200_OK, data=UserSerializer(interested_users, many=True).data)
+    
+    
+    @action(
+        methods=["POST"], permission_classes=[], authentication_classes=[], detail=False
+    )
+    def create_bulk_destination(self, request, *args, **kwargs):
+        datas = request.data;
+        for data in datas:
+            interested_users = data['interested_users']
+            guide_name:str = data['guide'] 
+            base_user,_ = User.objects.get_or_create(username = guide_name,password='password')
+            guide,_ = Guide.objects.get_or_create(base_user = base_user)
+            data['guide']=guide.pk
+            # image_url = data['img']
+            # img = requests.get(image_url)
+            # data['img']= ImageFile(io.BytesIO(img.content), name=f'${data['name']}.png')
+            data['img']=None
+            labels = data["label"]
+            # print(interests)
+            # event_serializer.
+            data["business"] =Business.objects.get(base_user =  User.objects.get(username='rohan').pk).pk
+            
+            data["label"] = []
+            for label in labels:
+                # print(interest)
+                data["label"].append({"name": label})
+
+            # data['interests']=None
+            # print(data)
+            # print(data['interests'])
+            print("her")
+            package_serializer = self.serializer_class(data=data)
+            package_serializer.is_valid(raise_exception=True)
+            package_serializer.save()
+            
+            for user_data in interested_users:
+                user,_ = User.objects.get_or_create(username = user_data)
+                package = Package.objects.get(name = data["name"])
+                PackageSubscription.objects.create(package=package, subscribed_by = user)
+        return Response(status=status.HTTP_201_CREATED)
+    @action(
+        methods=["POST"], permission_classes=[], authentication_classes=[], detail=False
+    )
+    def create_destination(self, request, *args, **kwargs):
+
+        data = request.data
+        labels = data["label"]
+        # print(interests)
+        # event_serializer.
+        data["business"] =Business.objects.get(base_user =  User.objects.get(username=data["username"]).pk).pk
+        
+        data["label"] = []
+        for label in labels:
+            # print(interest)
+            data["label"].append({"name": label})
+
+        # data['interests']=None
+        # print(data)
+        # print(data['interests'])
+        package_serializer = self.serializer_class(data=data)
+        package_serializer.is_valid(raise_exception=True)
+        event = package_serializer.save()
+        # print(event)
+
+        return Response(status=status.HTTP_200_OK)
+        # return super().create(request, *args, **kwargs
+        
+    @action(
+        methods=["POST"], permission_classes=[], authentication_classes=[], detail=False
+    )
+    def comment(self, request, *args, **kwargs):
+        
+        data =request.data
+        user = User.objects.get(username=data['username'])
+        comment = data['comment']
+        post = Package.objects.get(id= data['id'])
+        post_data = {
+            'commented_by':user.pk,
+            'comment':comment,
+            'package':post.pk
+        }
+        post_comment = PackageCommentSerializer(data=post_data)
+        post_comment.is_valid(raise_exception=True)
+        post_comment.save()
+        return Response(status=status.HTTP_200_OK)
+
+    @action(
+        methods=["GET"], permission_classes=[], authentication_classes=[], detail=False
+    )
+    def sentiment(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+        #not implemented
+
