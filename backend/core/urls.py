4c7f2da0cb4e93757bd6e89715bddc9b174d83b0
Rohan Acharya <rohanacharya38@gmail.com>
2025-01-04 08:52:34 +0545

    Added url and authentication file

---
+++ backend/core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PackageViewSet, TravellersViewSet,EventViewSet,PostViewSet,ChatbotViewSet,TraverseViewSet,GuideViewSet
+
+router = DefaultRouter()
+router.register(r'travellers', TravellersViewSet)
+router.register(r'events',EventViewSet)
+router.register(r'posts',PostViewSet)
+router.register(r'chat',ChatbotViewSet,basename="chat")
+router.register(r'destination',PackageViewSet)
+router.register(r'traverse', TraverseViewSet,basename="traverse")
+router.register(r'guide',GuideViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+]
