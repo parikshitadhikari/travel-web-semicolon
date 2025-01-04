c35da5523e9a299c537fd703c791b0b41092f37d
Rohan Acharya <rohanacharya38@gmail.com>
2025-01-04 13:17:31 +0545

    Added static urls to base url patterns

--- backend/travel_web/urls.py
+++ backend/travel_web/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('core.urls'))
     
 ]
+
+
+urlpatterns +=staticfiles_urlpatterns()
+urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
