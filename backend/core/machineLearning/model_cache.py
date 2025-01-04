ad571b3daa16cc50cf85791883743cc20396404c
Rohan Acharya <rohanacharya38@gmail.com>
2025-01-04 22:46:24 +0545

    uncommented model and tokenizer code

--- backend/core/machineLearning/model_cache.py
+++ backend/core/machineLearning/model_cache.py
@@ -8,13 +8,12 @@ class ModelCache:
 
     @classmethod
     def get_model(cls):
-        pass
-    #     if is_management_command():
-    #         return None  # Skip model loading during management commands
-    #     from tf_keras.models import load_model
-    #     from tf_keras.preprocessing.text import Tokenizer
-    #     if cls._model is None:
-    #         cls._model = load_model(cls._model_path)
-    #     return cls._model
+        if is_management_command():
+            return None  # Skip model loading during management commands
+        from tf_keras.models import load_model
+        from tf_keras.preprocessing.text import Tokenizer
+        if cls._model is None:
+            cls._model = load_model(cls._model_path)
+        return cls._model
 
 
