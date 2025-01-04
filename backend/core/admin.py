34c16eccf3be102fa72fe6787b7dac5f351e0c1f
Prayag Raj Acharya <prayagacharya.raj@gmail.com>
2025-01-04 08:53:29 +0545

    import models

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    PackageComment,
+    User,
+    Label,
+    Travellers,
+    Business,
+    Guide,
+    Package,
+    PackageSubscription,
+    Event,
+    EventInterested,
+    Post,
+    PostComment
+)
+
+from django.utils import timezone
+# Custom User admin (if needed for additional display configuration)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "email")
+
+    
