90cc80f69db154b1113d9180a428ab0231107566
Rohan Acharya <rohanacharya38@gmail.com>
2025-01-04 22:41:39 +0545

    Added chat gpt api

--- backend/core/machineLearning/tokenizer_cache.py
+++ backend/core/machineLearning/tokenizer_cache.py
@@ -10,13 +10,14 @@ class TokenizerCache:
 
     @classmethod
     def get_tokenizer(cls):
-        if is_management_command():
-            return None  # Skip tokenizer loading during management commands
-        from tf_keras.preprocessing.text import Tokenizer
-        if cls._tokenizer is None:
-            with open(cls._data_path, "r") as file:
-                data = [line.strip() for line in file]
-            tokenizer = Tokenizer(num_words=5000)
-            tokenizer.fit_on_texts(data)
-            cls._tokenizer = tokenizer
-        return cls._tokenizer
+        pass
+    #     if is_management_command():
+    #         return None  # Skip tokenizer loading during management commands
+    #     from tf_keras.preprocessing.text import Tokenizer
+    #     if cls._tokenizer is None:
+    #         with open(cls._data_path, "r") as file:
+    #             data = [line.strip() for line in file]
+    #         tokenizer = Tokenizer(num_words=5000)
+    #         tokenizer.fit_on_texts(data)
+    #         cls._tokenizer = tokenizer
+    #     return cls._tokenizer
