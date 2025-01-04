f2c82a62b769b97b67fc0d8257ed6cbda9148b16
Prayag Raj Acharya <prayagacharya.raj@gmail.com>
2025-01-04 09:02:17 +0545

    admin register label traveller and guide

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -23,4 +23,25 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "first_name", "last_name", "is_staff")
     search_fields = ("username", "email")
 
-    
+# Register Label
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+# Register Travellers
+@admin.register(Travellers)
+class TravellersAdmin(admin.ModelAdmin):
+    list_display = ("id", "base_user")
+    search_fields = ("base_user__username",)
+    filter_horizontal = ("interests",)
+
+# Register Business
+
+# Register Guide
+@admin.register(Guide)
+class GuideAdmin(admin.ModelAdmin):
+    list_display = ("id", "base_user__username", "base_user__email")
+    search_fields = ("base_user__username", "base_user__email")
+    filter_horizontal = ("label",)
