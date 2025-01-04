173e6766b89f1ec624ae95daea43572f03982a1c
Sudeep <i.sudeep.subedi@gmail.com>
2025-01-04 14:03:45 +0545

    Added the card component for traverse

--- frontend/src/components/ui/focus-cards.tsx
+++ frontend/src/components/ui/focus-cards.tsx
@@ -18,25 +18,59 @@ export const Card = React.memo(
     <div
       onMouseEnter={() => setHovered(index)}
       onMouseLeave={() => setHovered(null)}
-      className={cn(
-        "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
-        hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
-      )}
+      className="flex flex-col items-center w-full transition-all duration-300 ease-out"
     >
-      <Image
-        src={card.src}
-        alt={card.title}
-        fill
-        className="object-cover absolute inset-0"
-      />
       <div
         className={cn(
-          "absolute inset-0 bg-black/50 flex items-end py-8 px-4 transition-opacity duration-300",
-          hovered === index ? "opacity-100" : "opacity-0"
+          "relative rounded-xl bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full",
+          hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
         )}
       >
-        <div className="text-xl md:text-2xl font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">
-          {card.title}
+        <Image src={card.src} alt={card.title} fill className="object-cover" />
+        <div
+          className={cn(
+            "absolute bottom-0 left-0 right-0 bg-black/50 text-white text-lg font-semibold p-4 transition-opacity duration-300",
+            hovered === index ? "opacity-0" : "opacity-100"
+          )}
+        >
+          <div className="flex justify-between">
+            <div>{card.title}</div>
+            <p className="font-medium">${card.price}</p>
+          </div>
+        </div>
+        <div
+          className={cn(
+            "absolute inset-0 bg-black/80 flex flex-col justify-between items-start py-6 px-6 transition-opacity duration-300",
+            hovered === index ? "opacity-100" : "opacity-0"
+          )}
+        >
+          <div className="text-lg md:text-2xl font-semibold text-neutral-100 mt-2">
+            {card.title}
+          </div>
+          <div className="text-lg md:text-xl font-semibold text-neutral-100 mt-2">
+            ${card.price}
+          </div>
+          <div className="text-sm md:text-sm font-regular text-neutral-300 mt-2">
+            {card.description}
+          </div>
+          <div className="flex w-full">
+            <button
+              className={cn(
+                "mt-4 py-2 px-4 text-white bg-blue-500 rounded-full transition-opacity duration-300 w-full",
+                hovered === index ? "opacity-100" : "opacity-0"
+              )}
+            >
+              Buy
+            </button>
+            <button
+              className={cn(
+                "mt-4 py-2 px-4 text-blue-500 font-semibold hover:underline rounded-full transition-opacity duration-300 w-full",
+                hovered === index ? "opacity-100" : "opacity-0"
+              )}
+            >
+              View More
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -48,6 +82,8 @@ Card.displayName = "Card";
 type Card = {
   title: string;
   src: string;
+  price: number;
+  description: string;
 };
 
 export function FocusCards({ cards }: { cards: Card[] }) {
